a1e1fcefa6c6dd8f221f3cb22b0f9d38d88abd7e
Ivan Deiko <IvanDeiko97@gmail.com>
2019-08-25 19:18:55 +0300

    Updates

---
+++ precache-manifest.db8dc5b1acc3357d65e534cd93607252.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a966c9d37aaeb15da118a42b6858e9a",
+    "url": "/block2/index.html"
+  },
+  {
+    "revision": "de055a60a6a99a2d87af",
+    "url": "/block2/static/css/main.02844ea3.chunk.css"
+  },
+  {
+    "revision": "4b7b89cc65fa666ec60d",
+    "url": "/block2/static/js/2.daceb981.chunk.js"
+  },
+  {
+    "revision": "de055a60a6a99a2d87af",
+    "url": "/block2/static/js/main.e501439c.chunk.js"
+  },
+  {
+    "revision": "d6473da43334f3322685",
+    "url": "/block2/static/js/runtime~main.7c697042.js"
+  },
+  {
+    "revision": "4abbdfd32c05fb55317defec2d45dc25",
+    "url": "/block2/static/media/AudiType-Bold_03.4abbdfd3.woff"
+  },
+  {
+    "revision": "6890e6fade5253987ed3a29508fbce70",
+    "url": "/block2/static/media/AudiTypeScreen-Light.6890e6fa.woff"
+  },
+  {
+    "revision": "e13da8c77f4c9ac2ac04704a2cff83df",
+    "url": "/block2/static/media/backgroundForm.e13da8c7.png"
+  }
+]);
